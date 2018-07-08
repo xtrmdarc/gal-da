eef087af7248adad24d2d504442bbd8f62302763
Louis <anceun.1995@gmail.com>
2018-07-07 22:02:56 -0500

    Tablero Free
    Tablero Free 95%. :3

--- routes/Application/tablero_f/tablero_f_routes.php
+++ routes/Application/tablero_f/tablero_f_routes.php
@@ -1,4 +1,6 @@
 <?php
 
     Route::get('/tableroF', 'Application\TableroF\TableroFController@index')->name('tableroF');
+    Route::post('/graficoVentasyMonto', 'Application\TableroF\TableroFController@graficoVentasyMonto')->name('tableroF.gradicoVyM');
+
 ?>
