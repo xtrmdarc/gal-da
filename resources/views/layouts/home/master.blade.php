e755361571700bfcfc9306d10e3d75be36ef3177
Diego Reyes <diegoreyesco@hotmail.com>
2018-10-17 17:16:00 -0500

    chat online

--- resources/views/layouts/home/master.blade.php
+++ resources/views/layouts/home/master.blade.php
@@ -39,16 +39,24 @@
 
 <body>
 
-        @include('layouts.home.head')
-        @yield('content')
-        @include('layouts.home.footer')
+      @include('layouts.home.head')
+      @yield('content')
+      @include('layouts.home.footer')
 
     
-    <script type="text/javascript" src="{{ URL::to('home/js/bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::to('home/js/lightbox.min.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::to('home/js/wow.min.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::to('home/js/main.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::to('home/js/register/register-step.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::to('home/js/register/stripe.js') }}"></script>
+      <script type="text/javascript" src="{{ URL::to('home/js/bootstrap.min.js') }}"></script>
+      <script type="text/javascript" src="{{ URL::to('home/js/lightbox.min.js') }}"></script>
+      <script type="text/javascript" src="{{ URL::to('home/js/wow.min.js') }}"></script>
+      <script type="text/javascript" src="{{ URL::to('home/js/main.js') }}"></script>
+      <script type="text/javascript" src="{{ URL::to('home/js/register/register-step.js') }}"></script>
+      <script type="text/javascript" src="{{ URL::to('home/js/register/stripe.js') }}"></script>
+      <!-- Begin of Chaport Live Chat code -->
+      <script type="text/javascript">
+      (function(w,d,v3){
+      w.chaportConfig = { appId : '5bc779bcc3874a4eb186196c' };
+
+      if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document);
+      </script>
+      <!-- End of Chaport Live Chat code -->
 </body>
 </html>
