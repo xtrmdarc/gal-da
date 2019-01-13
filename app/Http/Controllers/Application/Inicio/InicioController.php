1c63db8730eaee4e17808b07c8bbee835c3c6068
Gal-Da <admin@gal-da.com>
2019-01-13 18:39:40 -0500

    Prueba en DD PreCuenta

--- app/Http/Controllers/Application/Inicio/InicioController.php
+++ app/Http/Controllers/Application/Inicio/InicioController.php
@@ -890,7 +890,8 @@ class InicioController extends Controller
             }
 
             //require_once __DIR__.'/public/rest/Imprimir/comp_pc.php';
-            require_once ('../public/rest/Imprimir/comp_pc.php');
+            dd( __DIR__,'../public/rest/Imprimir/comp_pc.php', __DIR__.'/public/rest/Imprimir/comp_pc.php');
+            //require_once ('../public/rest/Imprimir/comp_pc.php');
             return json_encode(1);
         }
         catch(Exception $e)
