4eca06deb9e42da44eb950102bd6550972db7050
Gal-Da <admin@gal-da.com>
2019-01-13 18:59:20 -0500

    DD PreCuentaTest

--- app/Http/Controllers/Application/Inicio/InicioController.php
+++ app/Http/Controllers/Application/Inicio/InicioController.php
@@ -890,8 +890,9 @@ class InicioController extends Controller
             }
 
             //require_once __DIR__.'/public/rest/Imprimir/comp_pc.php';
-            dd( __DIR__,'../public/rest/Imprimir/comp_pc.php', __DIR__.'/public/rest/Imprimir/comp_pc.php',app_path().'/public/rest/Imprimir/comp_pc.php');
+            //dd( __DIR__,'../public/rest/Imprimir/comp_pc.php', __DIR__.'/public/rest/Imprimir/comp_pc.php',app_path().'/public/rest/Imprimir/comp_pc.php');
             //require_once ('../public/rest/Imprimir/comp_pc.php');
+            require_once (app_path().'/public/rest/Imprimir/comp_pc.php');
             return json_encode(1);
         }
         catch(Exception $e)
