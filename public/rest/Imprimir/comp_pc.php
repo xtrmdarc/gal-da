9cdf28441ee19ba0c6651be174abe4039e618e90
Gal-Da <admin@gal-da.com>
2019-01-13 19:11:00 -0500

    DD PreCuenta Test 6

--- public/rest/Imprimir/comp_pc.php
+++ public/rest/Imprimir/comp_pc.php
@@ -1,7 +1,7 @@
 <?php
 //require_once __DIR__.'/public/rest/imprimir/num_letras.php';
 //require_once ('../public/rest/imprimir/num_letras.php');
-require_once (public_path().'/rest/imprimir/num_letras.php');
+require_once (public_path().'/rest/Imprimir/num_letras.php');
 //require_once __DIR__.'/public/rest/assets/pdf/cellfit.php';
 //require_once ('../public/rest/assets/pdf/cellfit.php');
 require_once (public_path().'/rest/assets/pdf/cellfit.php');
