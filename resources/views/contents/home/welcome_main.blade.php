789cbf23f0111a700378365e86356663d087e2ff
negocioizy <negocioizy@gmail.com>
2019-02-14 14:23:18 -0500

    fix imgs

--- resources/views/contents/home/welcome_main.blade.php
+++ resources/views/contents/home/welcome_main.blade.php
@@ -17,15 +17,15 @@
                         {{--<p>Boudin doner frankfurter pig. Cow shank bresaola pork loin tri-tip tongue venison pork belly meatloaf short loin landjaeger biltong beef ribs shankle chicken andouille.</p>--}}
                         <a href="/register" class="btn btn-common btn-cta-landing-main ">Empieza gratis</a>
                     </div>
-                    <img src="{{ URL::to('home/images/home/slider/sun.png') }}" class="slider-sun" alt="slider image">
-                    <img id="nubes-slider" src="{{ URL::to('home/images/home/slider/nubes.png') }}" class="slider-nubes" alt="slider image">
+                    <img src="{{ URL::to('home/images/home/slider/sun.png') }}" class="slider-sun" alt="sol silder">
+                    <img id="nubes-slider" src="{{ URL::to('home/images/home/slider/nubes.png') }}" class="slider-nubes" alt="nubes slider ">
                 
-                    <img id="zepellin-slider" src="{{ URL::to('home/images/home/slider/zepellin.png') }}" class="slider-zepellin" alt="slider image">
-                    <img src="{{ URL::to('home/images/home/slider/restaurante.png') }}" class="slider-hill" alt="slider image">
-                    <img src="{{ URL::to('home/images/home/slider/menu.png') }}" class="slider-house" alt="slider image">
+                    <img id="zepellin-slider" src="{{ URL::to('home/images/home/slider/zepellin.png') }}" class="slider-zepellin" alt="slider zepellin ">
+                    <img src="{{ URL::to('home/images/home/slider/restaurante.png') }}" class="slider-hill" alt="Tu restaurante gal-da">
+                    <img src="{{ URL::to('home/images/home/slider/menu.png') }}" class="slider-house" alt="menu del restaurante">
                     
-                    <img src="{{ URL::to('home/images/home/slider/birds1.png') }}" class="slider-birds1" alt="slider image">
-                    <img src="{{ URL::to('home/images/home/slider/birds2.png') }}" class="slider-birds2" alt="slider image">
+                    <img src="{{ URL::to('home/images/home/slider/birds1.png') }}" class="slider-birds1" alt="slider aves ">
+                    <img src="{{ URL::to('home/images/home/slider/birds2.png') }}" class="slider-birds2" alt="slider aves 2 ">
                     
                 </div>
             </div>
@@ -42,7 +42,7 @@
                     <p>Gal-Da es el sistema para restaurantes que te ayuda a automatizar y optimizar los procesos y tareas de tu restaurante, bar, café entre otros. Cuenta con un intuitivo POS (TPV) y herramientas administrativas.
                         </p>  
                     <div class="item active" style="margin-top:2em;">                             
-                        <img  src="{{URL::to('https://s3.amazonaws.com/galda-test-picture-empresas/tablero.png')}}" style="width:100%" alt="">
+                        <img  src="{{URL::to('https://s3.amazonaws.com/galda-test-picture-empresas/tablero.png')}}" style="width:100%" alt="tablero de control del sistema para restaurantes gratis gal-da">
                     </div>
                 </div>
             </div>
@@ -70,7 +70,7 @@
                 <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="300ms">
                     <div class="single-service">
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="300ms">
-                            <img src="{{ URL::to('home/images/home/monkey-use-landing-icon.jpg') }}" alt="">
+                            <img src="{{ URL::to('home/images/home/monkey-use-landing-icon.jpg') }}" alt="icono mono usando gal-da porque facil de usar ">
                         </div>
                         <h2>Fácil de usar</h2>
                         <p>Es intuitivo y cómodo de utilizar. Desarrollado a medida de la exigencia del mercado gastronómico.</p>
@@ -79,7 +79,7 @@
                 <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
                     <div class="single-service">
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="600ms">
-                            <img src="{{ URL::to('home/images/home/multiple-devices.jpg') }}" alt="">
+                            <img src="{{ URL::to('home/images/home/multiple-devices.jpg') }}" alt="dispositivos multiples en los que gal-da se puede usar">
                         </div>
                         <h2>Online</h2>
                         <p>Completamente en línea. No requiere instalación ni mantenimiento y se puede utilizar desde cualquier lugar.</p>
@@ -88,7 +88,7 @@
                 <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="900ms">
                     <div class="single-service">
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="900ms">
-                            <img src="{{ URL::to('home/images/home/metrics-landing-icon.png') }}" alt="">
+                            <img src="{{ URL::to('home/images/home/metrics-landing-icon.png') }}" alt="icono de informes realizados por el sistema gal-da">
                         </div>
                         <h2>Informes y reportes</h2>
                         <p>Información y estadística útil acerca del desempeño de tu restaurante para tomar decisiones.</p>
@@ -99,7 +99,7 @@
                 <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="300ms">
                     <div class="single-service">
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="300ms">
-                            <img src="{{ URL::to('home/images/home/zepelling-gratis.png') }}" alt="">
+                            <img src="{{ URL::to('home/images/home/zepelling-gratis.png') }}" alt="zepelling turquesa gratis">
                         </div>
                         <h2>Gratis</h2>
                         <p>Prueba nuestro plan completamente gratis con calidad empresarial.</p>
@@ -108,7 +108,7 @@
                 <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
                     <div class="single-service">
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="600ms">
-                            <img src="{{ URL::to('home/images/home/relax-movil-landing-icon.png') }}" alt="">
+                            <img src="{{ URL::to('home/images/home/relax-movil-landing-icon.png') }}" alt="sombrilla playa que refleja la libertad de quien usa gal-da">
                         </div>
                         <h2>Móvil</h2>
                         <p>Controla y monitorea las operaciones de tu restaurante desde cualquier parte con tu dispositivo móvil.</p>
@@ -117,7 +117,7 @@
                 <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="900ms">
                     <div class="single-service">
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="900ms">
-                            <img src="{{ URL::to('home/images/home/save-time-landing-icon.png') }}" alt="">
+                            <img src="{{ URL::to('home/images/home/save-time-landing-icon.png') }}" alt="alcancía ahorrador">
                         </div>
                         <h2>Ahorra tiempo</h2>
                         <p>Aumenta tu productividad optimizando los tiempos de las operaciones de tu negocio.</p>
@@ -154,7 +154,7 @@
             <div class="row">
                 <div class="single-features">
                     <div class="col-sm-5 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms" style="padding:0px;">
-                        <img src="{{ URL::to('home/images/home/galda-app.png') }}" class="img-responsive"  alt="">
+                        <img src="{{ URL::to('home/images/home/galda-app.png') }}" class="img-responsive"  alt="restaurante utiliza sistema gal-da">
                     </div>
                     <div class="col-sm-6 wow fadeInRight" style="margin-top:0px;" data-wow-duration="500ms" data-wow-delay="300ms">
                         <h2>Más rápido imposible.</h2>
@@ -193,7 +193,7 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="clients text-center wow fadeInUp" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <p><img src="{{ URL::to('home/images/home/clients.png') }}" class="img-responsive" alt=""></p>
+                        <p><img src="{{ URL::to('home/images/home/clients.png') }}" class="img-responsive" alt="clientes felices carrousel"></p>
                         <h2 class="title" style="font-size:3em">Clientes Felices</h2>
                         <p>Únete a nuestro grupo de cliente embajadores. <br> Y aprovecha grandes beneficios </p>
                     </div>
@@ -227,5 +227,9 @@
         </div>
     </section>
     <!--/#clients-->
-    <script src="{{URL::to('home/js/Pages/Landing_page.js')}}"></script>
+    
 @endsection('content')
+
+@section('scripts')
+    <script src="{{URL::to('home/js/Pages/Landing_page.js')}}"></script>
+@endsection
--- resources/views/layouts/home/master.blade.php
+++ resources/views/layouts/home/master.blade.php
@@ -1,40 +1,40 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      
+      <meta name="author" content="">
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      @yield('metas')
+      <title>Gal-Da | Sistema en línea para restaurantes </title>
+      <meta name="google-site-verification" content="Uwkj6rtsfo0SZxzCbHpV5ZCFJqj3YB7p4f1fXQ5V35U" />
+      <link href="{{ URL::to('home/css/bootstrap.min.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/font-awesome.min.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/animate.min.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/lightbox.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/main.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/responsive.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/auth/auth.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/home/styles.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/home/header.css') }}" rel="stylesheet">
+      <link href="{{ URL::to('home/css/home/pricing.css') }}" rel="stylesheet">
+      <!--[if lt IE 9]>
+      <script src="{{ URL::to('home/js/html5shiv.js') }}"></script>
+      <script src="{{ URL::to('home/js/respond.min.js') }}"></script>
+      <![endif]-->
+      <link rel="shortcut icon" href="{{ URL::to('https://s3.amazonaws.com/galda-test-picture-empresas/Vector+3.2.png') }}">
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::to('home/images/ico/apple-touch-icon-144-precomposed.png') }}">
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ URL::to('home/images/ico/apple-touch-icon-114-precomposed.png') }}">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::to('home/images/ico/apple-touch-icon-72-precomposed.png') }}">
+      <link rel="apple-touch-icon-precomposed" href="{{ URL::to('home/images/ico/apple-touch-icon-57-precomposed.png') }}">
     
-    <meta name="author" content="">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    @yield('metas')
-    <title>Gal-Da | Sistema en línea para restaurantes </title>
-    <meta name="google-site-verification" content="Uwkj6rtsfo0SZxzCbHpV5ZCFJqj3YB7p4f1fXQ5V35U" />
-    <link href="{{ URL::to('home/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/lightbox.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/main.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/responsive.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/auth/auth.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/home/styles.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/home/header.css') }}" rel="stylesheet">
-    <link href="{{ URL::to('home/css/home/pricing.css') }}" rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="{{ URL::to('home/js/html5shiv.js') }}"></script>
-    <script src="{{ URL::to('home/js/respond.min.js') }}"></script>
-    <![endif]-->
-    <link rel="shortcut icon" href="{{ URL::to('https://s3.amazonaws.com/galda-test-picture-empresas/Vector+3.2.png') }}">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::to('home/images/ico/apple-touch-icon-144-precomposed.png') }}">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ URL::to('home/images/ico/apple-touch-icon-114-precomposed.png') }}">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::to('home/images/ico/apple-touch-icon-72-precomposed.png') }}">
-    <link rel="apple-touch-icon-precomposed" href="{{ URL::to('home/images/ico/apple-touch-icon-57-precomposed.png') }}">
-    
-    <script type="text/javascript" src="{{ URL::to('home/js/jquery.js') }}"></script>
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Nunito:700,100,200,300">
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=PT+Sans:700,100,200,300,400">
-    <link rel="stylesheet"
+      
+      <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Nunito:700,100,200,300">
+      <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=PT+Sans:700,100,200,300,400">
+      <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Montserrat:700,100,200,300,400">
           <!-- Global site tag (gtag.js) - Google Analytics -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-49332747-5"></script>
@@ -72,7 +72,7 @@
       @include('layouts.home.head')
       @yield('content')
       @include('layouts.home.footer')
-
+      <script type="text/javascript" src="{{ URL::to('home/js/jquery.js') }}"></script>
     
       <script type="text/javascript" src="{{ URL::to('home/js/bootstrap.min.js') }}"></script>
       <script type="text/javascript" src="{{ URL::to('home/js/lightbox.min.js') }}"></script>
