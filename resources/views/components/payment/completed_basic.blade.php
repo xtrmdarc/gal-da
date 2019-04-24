eb53d63e7c96aa8cfea81d631cb4f1a9d313b36c
Gal-Da <admin@gal-da.com>
2019-04-24 16:01:16 -0500

    Completed Basic e imagen.

--- resources/views/components/payment/completed_basic.blade.php
+++ resources/views/components/payment/completed_basic.blade.php
@@ -1,143 +1,40 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title></title>
-</head>
-<body style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; background-color:#f0f2ea; margin:0; padding:0; color:#333333;">
+@extends('layouts.home.master_h_f_empty')
 
-<style>
-    .btn_verificar{
-        width: 100%;
-        text-transform: uppercase;
-        font-size: 14px;
-        padding: 15px;
-        border: 0px;
-        color: #fff;
-        background-color: #1fbdb7;
-        text-decoration: none;
-        font-family: 'PT Sans';
-        font-weight: 400;
-        line-height: 1.42857143;
-        text-align: center;
-        white-space: nowrap;
-        margin-left: 40%;
-    }
-</style>
+@section('content')
+    <style>
+        body,html{
+            height: 100%;
+        }
+        h1,h2,h3,h4,h5,h6{
+            font-family: 'Montserrat', sans-serif !important;
+        }
+    </style>
+    <link href="{{ URL::to('application/css/lib/sweetalert/sweetalert.css') }}" rel="stylesheet">
+    <div class="background-light-brand" style="height:100%">
+        <div class="background-light-brand">
+            <!-- Logo !-->
+            <div class="row "  >
+                <div class="col-sm-12" class="text-center">
+                    <img src="{{ URL::to('home/images/logo-1.png') }}" alt="galda-logo" class="img-responsive center-block section_landing" width="200px" height="80px">
+                </div>
+            </div>
+            <!-- Icon !-->
+            <div class="row section_landing">
+                <div class="col-sm-12" class="text-center">
+                    <img src="{{ URL::to('home/images/subscription/completed_basic.png') }}" alt="basic-logo" class="img-responsive center-block" >
+                </div>
+            </div>
+            <!-- Message and button !-->
+            <div class="row section_landing">
+                <div class=" col-sm-12 "   >
+                    <h2 class=" brand-color text-center center-block" style="font-weight:700;width:50%;">¡Felicidades!</h2>
+                    <h2 class=" brand-color text-center center-block" style="font-weight:700;width:50%;">Ya estas suscrito al Plan Basic</h2>
+                </div>
 
-<table width="100%" bgcolor="#f0f2ea" cellpadding="0" cellspacing="0" border="0">
-    <tbody>
-    <tr>
-        <td style="padding:40px 0;">
-            <!-- begin main block -->
-            <table cellpadding="0" cellspacing="0" width="608" border="0" align="center">
-                <tbody>
-                <tr>
-                    <td>
-                        <a href="http://ec2-18-207-223-230.compute-1.amazonaws.com" style="display:block; width:407px; height:100px; margin:0 auto 30px;">
-                            <img src="https://s3.amazonaws.com/galda-test-picture-empresas/Logo+1_1539157884.png" width="407" height="130" alt="gal-da" style="display:block; border:0; margin:0;">
-                        </a>
-                        <!-- begin wrapper -->
-                        <table cellpadding="0" cellspacing="0" border="0" width="100%">
-                            <tbody>
-                            <tr>
-                                <td width="8" height="4" colspan="2" style="background:url(http://demo.artlance.ru/email/shadow-top-left.png) no-repeat 100% 100%;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td height="4" style="background:url(http://demo.artlance.ru/email/shadow-top-center.png) repeat-x 0 100%;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td width="8" height="4" colspan="2" style="background:url(http://demo.artlance.ru/email/shadow-top-right.png) no-repeat 0 100%;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                            </tr>
-                            <tr>
-                                <td width="4" height="4" style="background:url(http://demo.artlance.ru/email/shadow-left-top.png) no-repeat 100% 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td colspan="3" rowspan="3" bgcolor="#FFFFFF" style="padding:0 0 30px;">
-                                    <!-- begin content -->
-                                    <p style="text-align:center; text-transform:uppercase; font-size:24px; line-height:30px; font-weight:bold; color:#484a42;">
-                                        Bienvenido a <b>BASIC</b>Gal-Da
-                                    </p>
-                                    <!-- begin articles -->
-                                    <table cellpadding="0" cellspacing="0" border="0" width="100%">
-                                        <tbody>
-                                        <tr valign="top">
-                                            <td width="30"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                            <td colspan="3">
-                                                <p style="font-size:14px; line-height:22px; font-weight:bold; color:#333333; margin:0 0 5px;margin-bottom: 25px;">Hola! Solo necesitamos verificar que tienes acceso a este correo. Termina de configurar tu cuenta haciendo clic en el boton de abajo</p>
-                                                <a style="width: 100%;
-                                            text-transform: uppercase;
-                                            font-size: 14px;
-                                            padding: 15px;
-                                            border: 0px;
-                                            color: #fff;
-                                            background-color: #1fbdb7;
-                                            text-decoration: none;
-                                            font-family: 'PT Sans';
-                                            font-weight: 400;
-                                            line-height: 1.42857143;
-                                            text-align: center;
-                                            white-space: nowrap;
-                                            margin-left: 40%;" href="{{route('sendEmailDone',["email" => $user->email, "verifyToken" => $user->verifyToken])}}">Verificar</a>
-                                            </td>
-                                            <td width="30"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                    <!-- /end articles -->
-                                    <p style="margin:0; border-top:2px solid #e5e5e5; font-size:5px; line-height:5px; margin:0 30px 29px;margin-top: 28px;">&nbsp;</p>
-                                    <table cellpadding="0" cellspacing="0" border="0" width="100%">
-                                        <tbody>
-                                        <tr valign="top">
-                                            <td width="30"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                            <td>
-                                                <p style="margin:0; color:#333333; font-size:11px; line-height:18px;">
-                                                    Surco, Lima, Perï¿½ <br>
-                                                    Website: <a href="http://www.gal-da.com" style="color:#6d7e44; text-decoration:none; font-weight:bold;">www.gal-da.com</a>
-                                                </p>
-                                                <p style="margin:0 0 4px; font-weight:bold; color:#333333; font-size:14px; line-height:22px;">@ Gal-Da 2019. Todos los derechos reservados.</p>
-                                            </td>
-                                            <td width="30"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                            <td width="120">
-                                                <a href="" style="float:left; width:24px; height:24px; margin:6px 8px 10px 0;">
-                                                    <img src="https://s3.amazonaws.com/galda-test-picture-empresas/facebook.png" width="24" height="24" alt="facebook" style="display:block; margin:0; border:0; background:#eeeeee;">
-                                                </a>
-                                            </td>
-                                            <td width="30"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                    <!-- end content -->
-                                </td>
-                                <td width="4" height="4" style="background:url(http://demo.artlance.ru/email/shadow-right-top.png) no-repeat 0 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                            </tr>
-
-
-                            <tr>
-                                <td width="4" style="background:url(http://demo.artlance.ru/email/shadow-left-center.png) repeat-y 100% 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td width="4" style="background:url(http://demo.artlance.ru/email/shadow-right-center.png) repeat-y 0 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                            </tr>
-
-                            <tr>
-                                <td width="4" height="4" style="background:url(http://demo.artlance.ru/email/shadow-left-bottom.png) repeat-y 100% 100%;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td width="4" height="4" style="background:url(http://demo.artlance.ru/email/shadow-right-bottom.png) repeat-y 0 100%;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                            </tr>
-
-                            <tr>
-                                <td width="4" height="4" style="background:url(http://demo.artlance.ru/email/shadow-bottom-corner-left.png) no-repeat 100% 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td width="4" height="4" style="background:url(http://demo.artlance.ru/email/shadow-bottom-left.png) no-repeat 100% 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td height="4" style="background:url(http://demo.artlance.ru/email/shadow-bottom-center.png) repeat-x 0 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td width="4" height="4" style="background:url(http://demo.artlance.ru/email/shadow-bottom-right.png) no-repeat 0 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                                <td width="4" height="4" style="background:url(http://demo.artlance.ru/email/shadow-bottom-corner-right.png) no-repeat 0 0;"><p style="margin:0; font-size:1px; line-height:1px;">&nbsp;</p></td>
-                            </tr>
-                            </tbody>
-                        </table>
-                        <!-- end wrapper-->
-                        <p style="margin:0; padding:34px 0 0; text-align:center; font-size:11px; line-height:13px; color:#333333;">
-                            No quieres recibir correos? Tu puedes desinscribirte <a href="#" style="color:#333333; text-decoration:underline;">aqui</a>
-                        </p>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-            <!-- end main block -->
-        </td>
-    </tr>
-    </tbody>
-</table>
-</body>
-</html>
+                <div class=" col-sm-12  "   >
+                    <h3 class="light-shade-color text-center center-block " style="width:40%; font-weight:300;"><a href="#" class="brand-color" >Inicia sesión</a>en tu Plan Basic.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
